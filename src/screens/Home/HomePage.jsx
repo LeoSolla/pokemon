dd2adbb4da62d4d14632dcf119bbd1f18755f4e0
Leonardo Solla <sollaleo@gmail.com>
2023-02-07 09:21:15 -0300

    landing page

--- src/screens/Home/HomePage.jsx
+++ src/screens/Home/HomePage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import pokemonLogo from '../../assets/images/pokemonLogo.png';
 
-const HomePage = () => (
-    <div className="home">
-        <div className="home__logo">
-            <img src={pokemonLogo} alt="Pokemon" />
+const HomePage = () => {
+    const history = useHistory();
+    
+    return (
+        <div className="home">
+            <div className="home__logo">
+                <img src={pokemonLogo} alt="Pokemon" />
+            </div>
+            <div className="home__start" onClick={() => history.push('/map')}>
+                start
+            </div>
         </div>
-        <div className="home__start">
-            start
-        </div>
-    </div>
-);
+    );
+};
 
 export default HomePage;
